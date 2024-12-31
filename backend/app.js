6b4f4255fa251310b7ebc6e89a15e8c7d3fd43e4
Jeetendra kumar <102826913+jeetendrakumar01@users.noreply.github.com>
2024-12-31 18:13:39 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ config({ path: "./config/config.env" });
 app.use(
   cors({
 
-    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+    origin: ["https://hospital-management-webapp.netlify.app/"],
 
     methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"],
     credentials: true,
